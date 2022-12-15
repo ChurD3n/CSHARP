e9a29b623a5f848826dc94787a7825c464a9686d
Denis <demid200479@gmail.com>
2022-12-15 21:42:10 +0300

    congratulation

---
+++ Example004_wassup/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("enter your name: ");
+string username = Console.ReadLine();
+
+if(username == "Maria")
+{ 
+Console.WriteLine("amazing you are Maria");    
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+ 
